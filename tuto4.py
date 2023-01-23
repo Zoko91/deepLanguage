aac5e5f7091b5ba7e93df2cd6ffa8b90ecceccef
Zoko91 <josephbeasse@gmail.com>
2023-01-23 18:19:35 +0100

    Modify models, not working - 25% proportions

--- tuto4.py
+++ tuto4.py
@@ -19,8 +19,40 @@ test = data.skip(34000).take(6000)
 
 
 # --------------------- Build the model ---------------------
+# model = tf.keras.Sequential()
+# model.add(tf.keras.layers.Reshape((153, 13, 1), input_shape=(1, 153, 13)))
+# model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+# model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+# model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+# model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+# model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+# model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+# model.add(tf.keras.layers.Flatten())
+# model.add(tf.keras.layers.Dense(64, activation='relu'))
+# model.add(tf.keras.layers.Dense(4, activation='softmax'))
+
+# model = tf.keras.Sequential()
+# # Input reshape
+# model.add(tf.keras.layers.Reshape((153, 13, 1), input_shape=(1, 153, 13)))
+# # Convolutional layers
+# model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same'))
+# model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 1)))
+# model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same'))
+# model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 1)))
+# model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
+# model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 1)))
+# # Fully connected layers
+# model.add(tf.keras.layers.Flatten())
+# model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+# model.add(tf.keras.layers.Dense(units=32, activation='relu'))
+# # Output layer
+# model.add(tf.keras.layers.Dense(units=4, activation='softmax'))
 
 
-# --------------------- Train the model ---------------------
+# #  Compile the model
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
+# model.summary()
+#
+# # --------------------- Train the model ---------------------
 # history = model.fit(train, epochs=10, validation_data=test, verbose=1)
 
