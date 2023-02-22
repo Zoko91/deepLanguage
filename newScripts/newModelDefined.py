5539819fdedcb2b4fb2bf08dc1e22779f24c6533
Zoko91 <josephbeasse@gmail.com>
2023-02-22 12:00:21 +0100

    new model

--- newScripts/newModelDefined.py
+++ newScripts/newModelDefined.py
@@ -7,12 +7,13 @@ data = tf.data.Dataset.load('../Models/newData')
 
 # --------------------- Prepare the data ---------------------
 # Shuffle the data
-data = data.shuffle(60000)
+data = data.shuffle(50000)
 data = data.batch(64)
 data = data.prefetch(32)
 # Split the data into training and validation sets
 # Train has 34000 samples, test has 6000 samples
-train = data.take(40000)
+train = data.take(34000)
+test = data.skip(34000).take(6000)
 
 # Model N°2
 # -----------------------------------------------------------
@@ -28,18 +29,25 @@ model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(tf.keras.layers.BatchNormalization())
 model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+
 # Fully connected layers
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+model.add(tf.keras.layers.Dense(units=512, activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
 # Output layer
 model.add(tf.keras.layers.Dense(units=4, activation='softmax'))
-model.add(tf.keras.layers.Reshape((1, 4)))
+model.add(tf.keras.layers.Reshape((1, 4))) # match the labels which are [[0,0,0,1]] distribution over 4 languages
 
 #  Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
 # --------------------- Train the model ---------------------
-history = model.fit(train, epochs=10, verbose=1)
-model.save('../Models/modelNew.h5')
+history = model.fit(train,validation_data=test, epochs=20, verbose=1)
+model.save('../Models/model2.h5')
+
+
+# --------------------- Model2 ---------------------
+# Epoch 20/20
+# 688/688 [==============================] - 52s 74ms/step - loss: 0.1367 - accuracy: 0.9519
