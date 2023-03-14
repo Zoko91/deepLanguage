5c6aa146fa95aafe925d2841bf65e8dd06c6a435
Zoko91 <josephbeasse@gmail.com>
2023-03-14 08:32:07 +0100

    Models

---
+++ modelCreation.py
@@ -0,0 +1,68 @@
+# --------------------- Playground ---------------------
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# --------------------- Load the data ---------------------
+train_dataset = tf.data.Dataset.load('./newData/train_dataset').batch(32).prefetch(16)
+val_dataset = tf.data.Dataset.load('./newData/validation_dataset').batch(32).prefetch(16)
+
+
+# --------------------- Model --------------------------------
+model = tf.keras.Sequential()
+
+# Input layer
+model.add(tf.keras.layers.Input(shape=(13, 157, 1)))
+
+# Convolutional layers
+model.add(tf.keras.layers.Conv2D(filters=8, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+
+# Fully connected layers
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(units=512, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(units=256, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)))
+model.add(tf.keras.layers.Dropout(0.3))
+model.add(tf.keras.layers.Dense(units=128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)))
+model.add(tf.keras.layers.Dropout(0.2))
+
+# Output layer
+model.add(tf.keras.layers.Dense(units=4, activation='softmax'))
+
+# Compile the model
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+# Early stopping
+early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+
+# Train the model
+history = model.fit(train_dataset, epochs=20, validation_data=val_dataset, callbacks=[early_stopping_callback],verbose=1)
+model.save('./newData/model.h5')
+
+
+# Plot the loss
+plt.plot(history.history['loss'], label='train_loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(loc='upper right')
+plt.show()
+
+# Plot the accuracy
+plt.plot(history.history['accuracy'], label='train_acc')
+plt.plot(history.history['val_accuracy'], label='val_acc')
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(loc='lower right')
+plt.show()
